c4be059d182a90cfc88d34f8f6da5068b25c1708
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2019-08-20 00:18:23 +0200

    Reduced number of API calls

--- custom_components/burze_dzis_net/binary_sensor.py
+++ custom_components/burze_dzis_net/binary_sensor.py
@@ -1,11 +1,12 @@
-from zeep import Client
-from zeep.exceptions import Fault
+from datetime import timedelta
 import voluptuous as vol
 import logging
 
+from homeassistant.util import Throttle
 from homeassistant.util.dt import parse_datetime
 from homeassistant.components.binary_sensor import PLATFORM_SCHEMA, ENTITY_ID_FORMAT
-from homeassistant.const import CONF_NAME, CONF_RADIUS, CONF_API_KEY, ATTR_ATTRIBUTION, CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.const import CONF_NAME, CONF_RADIUS, CONF_API_KEY, ATTR_ATTRIBUTION, CONF_LATITUDE, CONF_LONGITUDE, \
+    CONF_SCAN_INTERVAL
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.helpers.entity import async_generate_entity_id
@@ -16,15 +17,16 @@ CONF_WARNINGS = 'warnings'
 CONF_STORMS_NEARBY = 'storms_nearby'
 
 DEFAULT_NAME = 'Burze.dzis.net'
+DEFAULT_SCAN_INTERVAL = timedelta(minutes=2, seconds=30)
 ATTRIBUTION = 'Information provided by Burze.dzis.net.'
 
 WARNING_TYPES = {
-    'frost_warning': ['mroz', 'Ostrzeżenie - Mróz'],
-    'heat_warning': ['upal', 'Ostrzeżenie - Upał'],
-    'wind_warning': ['wiatr', 'Ostrzeżenie - Wiatr'],
-    'precipitation_warning': ['opad', 'Ostrzeżenie - Opad'],
-    'storm_warning': ['burza', 'Ostrzeżenie - Burza'],
-    'tornado_warning': ['traba', 'Ostrzeżenie - Trąba'],
+    'frost_warning': ['mroz', 'Ostrzeżenie - Mróz', 'mdi:weather-snowy'],
+    'heat_warning': ['upal', 'Ostrzeżenie - Upał', 'mdi:weather-sunny'],
+    'wind_warning': ['wiatr', 'Ostrzeżenie - Wiatr', 'mdi:weather-windy'],
+    'precipitation_warning': ['opad', 'Ostrzeżenie - Opad', 'mdi:weather-pouring'],
+    'storm_warning': ['burza', 'Ostrzeżenie - Burza', 'mdi:weather-lightning-rainy'],
+    'tornado_warning': ['traba', 'Ostrzeżenie - Trąba', 'mdi:weather-hurricane'],
 }
 WARNING_DESCRIPTIONS = {
     'frost_warning': {
@@ -58,7 +60,7 @@ WARNING_DESCRIPTIONS = {
         3: "ryzyko wysokie"
     }
 }
-STORM_NEARBY = 'Burze w pobliżu'
+STORM_NEARBY = ['Burze w pobliżu', 'mdi:weather-lightning']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_LATITUDE): cv.string,
@@ -70,59 +72,49 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_STORMS_NEARBY):
         vol.Schema({
             vol.Required(CONF_RADIUS): cv.positive_int
-        })
+        }),
+    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period
 })
-
-
-def get_service():
-    return Client('https://burze.dzis.net/soap.php?WSDL').service
 
 
 def convert_to_dm(dmf):
     return '{}.{:02}'.format(int(dmf), round(dmf % 1 * 60))
 
 
-def check_connection(api_key):
-    try:
-        get_service().ostrzezenia_pogodowe('0', '0', api_key)
-    except Fault as fault:
-        raise 'Error setting up burze_dzis_net: {}'.format(fault.message)
-
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
     latitude = float(config.get(CONF_LATITUDE, hass.config.latitude))
     longitude = float(config.get(CONF_LONGITUDE, hass.config.longitude))
     api_key = config.get(CONF_API_KEY)
     warnings = config.get(CONF_WARNINGS)
     storms_nearby = config.get(CONF_STORMS_NEARBY)
+    scan_interval = config.get(CONF_SCAN_INTERVAL)
     radius = 0
     if storms_nearby is not None:
         radius = storms_nearby.get(CONF_RADIUS)
-    check_connection(api_key)
     sensors = []
     sensor_name = '{} - '.format(name)
     x = convert_to_dm(longitude)
     y = convert_to_dm(latitude)
+    updater = BurzeDzisNetDataUpdater(x, y, radius, api_key, scan_interval)
+    await updater.async_update()
     for warning_type in warnings:
         uid = '{}_{}'.format(name, warning_type)
         entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, uid, hass=hass)
-        sensors.append(BurzeDzisNetWarningsSensor(entity_id, sensor_name, x, y, api_key, warning_type))
+        sensors.append(BurzeDzisNetWarningsSensor(entity_id, sensor_name, updater, warning_type))
     if storms_nearby is not None:
         uid = '{}_storms_nearby'.format(name)
         entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, uid, hass=hass)
-        sensors.append(BurzeDzisNetStormsNearbySensor(entity_id, sensor_name, x, y, api_key, radius))
-    add_entities(sensors, True)
+        sensors.append(BurzeDzisNetStormsNearbySensor(entity_id, sensor_name, updater))
+    async_add_entities(sensors, True)
 
 
 class BurzeDzisNetSensor(BinarySensorDevice):
 
-    def __init__(self, entity_id, name, x, y, api_key):
+    def __init__(self, entity_id, name, updater):
         self.entity_id = entity_id
         self._name = name
-        self._x = x
-        self._y = y
-        self._api_key = api_key
+        self._updater = updater
         self._data = None
 
     @property
@@ -131,57 +123,86 @@ class BurzeDzisNetSensor(BinarySensorDevice):
         output[ATTR_ATTRIBUTION] = ATTRIBUTION
         return output
 
+    async def async_update(self):
+        await self._updater.async_update()
+
 
 class BurzeDzisNetWarningsSensor(BurzeDzisNetSensor):
-    def __init__(self, entity_id, name, x, y, api_key, warning_type):
-        super().__init__(entity_id, name, x, y, api_key)
+    def __init__(self, entity_id, name, updater, warning_type):
+        super().__init__(entity_id, name, updater)
         self._warning_type = warning_type
         self._warning_key = WARNING_TYPES[self._warning_type][0]
 
     @property
     def is_on(self):
-        return self._data is not None and self._data[self._warning_key] > 0
+        data = self._updater.ostrzezenia_pogodowe_output
+        return data is not None and data[self._warning_key] > 0
 
     @property
     def device_state_attributes(self):
         output = super().device_state_attributes
         if self.is_on:
-            output['level'] = self._data[self._warning_key]
-            output['description'] = WARNING_DESCRIPTIONS[self._warning_type][self._data[self._warning_key]]
-            output['from'] = str(parse_datetime(self._data[self._warning_key + '_od_dnia'] + 'Z'))
-            output['to'] = str(parse_datetime(self._data[self._warning_key + '_do_dnia'] + 'Z'))
+            data = self._updater.ostrzezenia_pogodowe_output
+            output['level'] = data[self._warning_key]
+            output['description'] = WARNING_DESCRIPTIONS[self._warning_type][data[self._warning_key]]
+            output['from'] = str(parse_datetime(data[self._warning_key + '_od_dnia'] + 'Z'))
+            output['to'] = str(parse_datetime(data[self._warning_key + '_do_dnia'] + 'Z'))
         return output
+
+    @property
+    def icon(self):
+        return WARNING_TYPES[self._warning_type][2]
 
     @property
     def name(self):
         return self._name + WARNING_TYPES[self._warning_type][1]
 
-    def update(self):
-        self._data = get_service().ostrzezenia_pogodowe(self._y, self._x, self._api_key)
-
 
 class BurzeDzisNetStormsNearbySensor(BurzeDzisNetSensor):
-    def __init__(self, entity_id, name, x, y, api_key, radius):
-        super().__init__(entity_id, name, x, y, api_key)
-        self._radius = radius
+    def __init__(self, entity_id, name, updater):
+        super().__init__(entity_id, name, updater)
 
     @property
     def is_on(self):
-        return self._data is not None and self._data['liczba'] > 0
+        data = self._updater.szukaj_burzy_output
+        return data is not None and data['liczba'] > 0
 
     @property
     def device_state_attributes(self):
         output = super().device_state_attributes
         if self.is_on:
-            output['number'] = self._data['liczba']
-            output['distance'] = self._data['odleglosc']
-            output['direction'] = self._data['kierunek']
-            output['period'] = self._data['okres']
+            data = self._updater.szukaj_burzy_output
+            output['number'] = data['liczba']
+            output['distance'] = data['odleglosc']
+            output['direction'] = data['kierunek']
+            output['period'] = data['okres']
         return output
 
     @property
     def name(self):
-        return self._name + STORM_NEARBY
+        return self._name + STORM_NEARBY[0]
 
-    def update(self):
-        self._data = get_service().szukaj_burzy(self._y, self._x, self._radius, self._api_key)
+    @property
+    def icon(self):
+        return STORM_NEARBY[1]
+
+
+class BurzeDzisNetDataUpdater:
+    def __init__(self, x, y, radius, api_key, scan_interval):
+        self._x = x
+        self._y = y
+        self._radius = radius
+        self._api_key = api_key
+        self.szukaj_burzy_output = None
+        self.ostrzezenia_pogodowe_output = None
+        self.async_update = Throttle(scan_interval)(self._async_update)
+
+    async def _async_update(self):
+        from zeep import Client
+        from zeep.exceptions import Fault
+        service = Client('https://burze.dzis.net/soap.php?WSDL').service
+        try:
+            self.ostrzezenia_pogodowe_output = service.ostrzezenia_pogodowe(self._y, self._x, self._api_key)
+            self.szukaj_burzy_output = service.szukaj_burzy(self._y, self._x, self._radius, self._api_key)
+        except Fault as fault:
+            _LOGGER.error('Error setting up burze_dzis_net: {}', fault)
